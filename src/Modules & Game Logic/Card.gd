ca9056e038c82b3a6c7e674cb4ca1e5a0d6e6791
Surya <suryakrish1123@gmail.com>
2026-02-19 22:33:14 -0800

    added functions to deck_system.gd

--- src/Modules & Game Logic/Card.gd
+++ src/Modules & Game Logic/Card.gd
@@ -4,17 +4,18 @@ class_name Cards
 # (abstract class)
 
 # list of parameters:
-var art = null
-var cost: int
-var type: String
+var art : Texture2D
+var cost: int = 10
+enum type_of_card {attack, defense, status, power}
+var type: type_of_card 
 var name: String = "bob"
-var identifier = null
+var identifier: String = "id1"
 var level: int = 1
-var recharge_time: float
-var description: String
-var target: Array  = []
-var tier: int
+var recharge_time: float = 5.0
+var description: String = ""
+var target: int = 1
+var tier: int = 1
 
-## func activate()
+## func activate() ## Not implemented in this class
 
-## func select_targets()
+## func select_targets() ## helper function to assign target
--- src/Modules & Game Logic/deck_system.gd
+++ src/Modules & Game Logic/deck_system.gd
@@ -1,7 +1,42 @@
 extends Node
-var deck = []
-var hand = []
+class_name Deck
 
-func shuffle_deck():
-	deck.shuffle()
+##var deck = []
+## var hand = []
+var cards = []
+var removed_card = []
+
+## func is_empty():
+##	if cards.empty():
+##		return
+		
+func draw_card():
+	if cards.empty():
+		return
+	return cards.pop_front()
+
+func discard_card(card):
+	removed_card = removed_card + [card]
+	
+func add_card(card):
+	cards = cards + [card]
+
+func remove_card(card):
+	cards.erase(card)
+	
+func reset_card_deck():
+	cards = cards + removed_card
+	shuffle_card_deck()
+	
+func shuffle_card_deck():
+	cards.shuffle()
 	print("Deck has been shuffled.")
+
+func obtain_top_card():
+	if cards.empty():
+		return
+	else:
+		return(cards[0])
+
+func duplicate_card(card):
+	return(card.duplicate(true))
