3dc768abcea2d4c2afd98c157db80cb6d75f44a8
Dorianv5 <118552196+Dorianv5@users.noreply.github.com>
2026-02-26 20:18:03 -0800

    Hopefully fixed all merge conflicts and all editor warnings

--- src/Cards/_Template/card_scene.gd
+++ src/Cards/_Template/card_scene.gd
@@ -1,37 +1,40 @@
-@tool
 extends Node2D
 class_name Card
+
+@onready var card_art: TextureRect = $PanelContainer/MarginContainer/VBoxContainer/Control/CardArt
+@onready var name_label = $PanelContainer/MarginContainer/VBoxContainer/Name
+@onready var card_description = $PanelContainer/MarginContainer/VBoxContainer/Description
+@onready var type = $PanelContainer/MarginContainer/VBoxContainer/Control/Type
 
 @export_enum("Spell", "Attack") var card_type = "Not Set":
 	set(t):
 		if Engine.is_editor_hint():
 			card_type = t
-			type.text = str(card_type)
+			if type != null:
+				type.text = str(card_type)
 
 @export var card_name: String = "Name":
 	set(t):
 		if Engine.is_editor_hint():
 			card_name = t
-			name_label.text = card_name
+			if name_label != null:
+				name_label.text = card_name
 
 @export var art: Texture2D = load("res://icon.svg"):
 	set(t):
 		if Engine.is_editor_hint():
 			art = t
-			card_art.texture = art
+			if card_art != null:
+				card_art.texture = art
 
 @export_multiline var description: String = "Description":
 	set(t):
 		if Engine.is_editor_hint():
 			description = t
-			card_description.text = description
+			if card_description != null:
+				card_description.text = description
 
 @export var vfx_scene: PackedScene
-
-@onready var card_art: TextureRect = $PanelContainer/MarginContainer/VBoxContainer/CardArt
-@onready var name_label = $PanelContainer/MarginContainer/VBoxContainer/Name
-@onready var card_description = $PanelContainer/MarginContainer/VBoxContainer/Description
-@onready var type = $PanelContainer/MarginContainer/VBoxContainer/Control/Type
 
 var tween: Tween
 
@@ -39,6 +42,10 @@ func activate():
 	push_error("ACTIVATE NOT IMPLEMENTED: extend this script and implement activate()")
 
 func _ready():
+	type.text = str(card_type)
+	name_label.text = card_name
+	card_art.texture = art
+	card_description.text = description
 	$PanelContainer.mouse_entered.connect(hover)
 	$PanelContainer.mouse_exited.connect(end_hover)
 
