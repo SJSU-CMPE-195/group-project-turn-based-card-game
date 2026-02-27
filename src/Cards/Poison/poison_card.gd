543de7e7845fdf5ae314bc0c7ef480caeb3a6bfd
vincentc168777 <vinbest777@gmail.com>
2026-02-26 21:39:26 -0800

    added units code

---
+++ src/Cards/Poison/poison_card.gd
@@ -0,0 +1,16 @@
+@tool
+extends Card
+
+func activate():
+	var instance = vfx_scene.instantiate()
+	CombatManager.selected_ally.add_child(instance) ## adds the vfx as a child of the ally unit
+	instance.global_position -= Vector2(0.0, 50.0)
+	instance.set_target(CombatManager.selected_enemy) ## set target of the attack, attack animates to target position
+	instance.play() ## starts the vfx animation
+	await instance.target_reached
+	CombatManager.selected_enemy.take_damage(20)
+	
+
+func _input(event):
+	if event.is_action_pressed("PoisonTest"): ## Activates when input is "Selected"
+		activate()
