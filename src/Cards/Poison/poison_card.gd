24d874de75685dbf92c95297b116073cae830fd9
vincentc168777 <vinbest777@gmail.com>
2026-02-26 22:40:52 -0800

    poison card

--- src/Cards/Poison/poison_card.gd
+++ src/Cards/Poison/poison_card.gd
@@ -1,11 +1,16 @@
-extends Node
+@tool
+extends Card
 
+func activate():
+	var instance = vfx_scene.instantiate()
+	CombatManager.selected_ally.add_child(instance) ## adds the vfx as a child of the ally unit
+	instance.global_position -= Vector2(0.0, 50.0)
+	instance.set_target(CombatManager.selected_enemy) ## set target of the attack, attack animates to target position
+	instance.play() ## starts the vfx animation
+	await instance.target_reached
+	CombatManager.selected_enemy.take_damage(20)
+	
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event):
+	if event.is_action_pressed("PoisonTest"): ## Activates when input is "Selected"
+		activate()
