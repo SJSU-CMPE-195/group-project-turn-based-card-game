e6597d6e8fd0706a480a1a7d9947bebca25a34bb
Surya <suryakrish1123@gmail.com>
2026-03-07 20:12:24 -0800

    fix unit errors, begin card/upgrade shop logic

---
+++ src/Modules & Game Logic/Card_shop.gd
@@ -0,0 +1,4 @@
+extends Node
+class_name Card_shop
+@export var product: Data_Card
+# description: logic for card shop
---
+++ src/Modules & Game Logic/Upgrade_shop.gd
@@ -0,0 +1,4 @@
+extends Node
+class_name Upgrade_shop
+@export 
+# description: logic for upgrade shop
