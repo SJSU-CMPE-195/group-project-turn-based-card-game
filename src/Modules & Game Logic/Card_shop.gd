a185f2f103aa44f88240a5e05fbdf8a65b9de6d3
Surya <suryakrish1123@gmail.com>
2026-03-19 16:09:23 -0700

    integrated frontend with backend for card/upgrade shop

--- src/Modules & Game Logic/Card_shop.gd
+++ src/Modules & Game Logic/Card_shop.gd
@@ -3,9 +3,12 @@ class_name Card_shop
 # description: logic for card shop
 
 @export var product: Data_Card
+@export var fireball: Data_Card
+@export var shield: Data_Card
+@export var heal: Data_Card
 @export var gold: int
 @export var cardupgrade: Data_Card
-@export var upgrade_price: int = 100
+@export var upgrade_price: int = 400
 
 func product_found():
 	if(product != null):
@@ -23,26 +26,47 @@ func buy():
 
 func press_buy():
 	var bought = buy()
-	if bought == null:
-		print("Card does not exist or you don't have sufficient gold to buy it")
-	else:
-		print(bought.card_name, " has been purchased")
+	if (product.money > gold):
+		print("Not sufficient gold to purchase ", product.card_name)
+	elif(gold >= product.money):
+		print(bought.card_name, " has been purchased, you have ", gold, " gold remaining")
 
 func press_upgrade():
-	if (upgrade_price > gold or product == null):
-		print("Product not found or not sufficient gold to upgrade")
+	if(product == null):
+		print("Product not found")
+	elif (upgrade_price > gold):
+		print("Not sufficient gold to upgrade")
 		return null
 	elif(gold >= upgrade_price):
+		gold = gold - upgrade_price
 		product.upgrade()
-		print("Succesfully upgraded", product.card_name)
+		print(product.card_name, " has been upgraded, you have ", gold, " gold remaining")
 		return(product)
 	
 func display():
 	return{"Name of card is": product.card_name, "Price of card is": product.money,
 	"Card description": product.description}
 	
-func _on_buy_card_pressed() -> void:
+func _on_button_to_buy_fireball_pressed() -> void:
+	product = fireball
 	press_buy()
 
-func _on_card_to_upgrade_pressed() -> void:
+func _on_button_to_buy_shield_pressed() -> void:
+	product = shield
+	press_buy()
+	
+func _on_button_to_buy_heal_pressed() -> void:
+	product = heal
+	press_buy()
+
+func _on_button_to_upgrade_shield_pressed() -> void:
+	product = shield
+	press_upgrade()
+
+func _on_button_to_upgrade_heal_pressed() -> void:
+	product = heal
+	press_upgrade()
+
+func _on_button_to_upgrade_fireball_pressed() -> void:
+	product = fireball
 	press_upgrade()
